aafc48746f646f60fe22a1a153c504246728afe5
amandamacz <108598076+amandamacz@users.noreply.github.com>
2022-12-01 21:10:39 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function SaveItem(){
+    var name = document.forms.ShoppingList.name.value;
+    var data = document.forms.ShoppingList.data.value;
+    localStorage.setItem(name, data);
+    ShowAll();
+
+}
+
+function ModifyItem(){
+    localStorage.getItem(name);
+    document.forms.ShoppingList.data.value = localStorage.getItem(name);
+    localStorage.getItem(name);
+    ShowAll();
+}
+
+function RemoveItem() {
+    var name = document.forms.ShoppingList.name.value;
+    document.forms.ShoppingList.data.value = localStorage.removeItem(name);
+    localStorage.removeItem(name);
+    ShowAll();
+
+}
+
+function ClearAll() {
+    localStorage.clear();
+    ShowAll();
+
+}
+
+function ShowAll() {
+    if (CheckBrowser()){
+        var key ="";
+        var list="";
+        var i=0;
+        for (i=0; i<=localStorage.length - 1; i++){
+            key = localStorage.key(i);
+            list +="<tr><td>" + key + "</td>\n<td>" + localStorage.getItem(key) + "</td></tr>\n";
+        }
+        document.getElementById('list').innerHTML = list;
+    }else{
+        alert("Cannot store shopping list. Your browser does not support local storage")
+    }
+    }
+
+
+function CheckBrowser() {
+    if ('localStorage' in window && window ['localStorage'] !==null){
+        //we can use local storage
+        console.log ('localStorage works');
+        return true;
+    }else{
+        return false;
+    }
+    }
